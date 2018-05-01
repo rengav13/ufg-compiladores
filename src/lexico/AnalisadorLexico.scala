a19522ebc8c75984f27ed3ff40c6915b438f3609
chris kalombo <38844201+chriskalombo1@users.noreply.github.com>
2018-05-01 11:01:42 -0300

    Comentarios

--- src/lexico/AnalisadorLexico.scala
+++ src/lexico/AnalisadorLexico.scala
@@ -7,21 +7,25 @@ class AnalisadorLexico(fonte: String) {
   var linha: Int = 0
   var coluna: Int = 0
 
+  //Percorrendo o arquivo caractere por caractere, associando ele a um simbolo identificado usando a Função ToSimbolo:
   def proximoToken(): Token = {
     var char: Char = arquivo.next()
     var simbolo: String = this.toSimbolo(char)
     var lexema: String = char.toString
 
+    // Enquanto o lexema não for classificado 
     while (!ControladorAutomato.isLexemaClassificado) {
-      char = arquivo.next()
-      simbolo = this.toSimbolo(char)
-      this.atualizaPosicao(char)
-      lexema += char.toString
-      ControladorAutomato.vaiParaProximoEstado(simbolo)
+      char = arquivo.next()                //Lendo por caracteres de cada Token        
+      simbolo = this.toSimbolo(char)       //Passando o simbolo definido para o tipo de caracter lido
+      this.atualizaPosicao(char)           //Ir para a próxima posição 
+      lexema += char.toString              //lexema recebe a string dos caracteres lidos
+      
+      ControladorAutomato.vaiParaProximoEstado(simbolo)   //Com o simbolo o controladorAutomato nos passa o próximo estado do Automato
     }
-    new Token(ControladorAutomato.getTipoToken, lexema)
+    new Token(ControladorAutomato.getTipoToken, lexema)  //Associando o tipo do Token ao lexema
   }
 
+  //Função para categorizar o tipo de caracter lido assiciando eles a um simbolo  
   def toSimbolo(char: Char): String = {
     char match {
       case _ if Character.isDigit(char) => "D"
