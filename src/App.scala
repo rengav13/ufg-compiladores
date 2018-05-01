da05f3eb140da62557651eead6aa557ed5c4f502
chris kalombo <38844201+chriskalombo1@users.noreply.github.com>
2018-05-01 09:37:02 -0300

    Update App.scala

--- src/App.scala
+++ src/App.scala
@@ -1,9 +1,12 @@
 import lexico.AnalisadorLexico
 
+//Localizando e Carregando o arquivo .txt da linguagem Mgol a ser analizado
+
 object App {
   def main(args: Array[String]) {
     val lexico = new AnalisadorLexico("D:\\Universidade\\9_semestre\\Compiladores\\Projeto\\ufg-compiladores\\compilador\\src\\codigo_teste.mgol")
-
+    
+ //Enquanto estiver lendo o arquivo: quit= para sair  
     while (true) {
       Console.readLine match {
         case "quit" => System.exit(0)
